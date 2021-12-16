edca2708a7d03c57ba793fb67ff181954beb5d57
kekeimiku <kekelanact@gmail.com>
2021-12-16 09:48:50 +0800

    1

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+	"ykeysnail/window/wayland"
+)
 
+func main() {
+	go func() {
+		for {
+			select {
+			case event := <-wayland.SubscriptionSwayIPC.Events:
+				if event.Container.AppID == "Alacritty" && event.Container.Name == "Alacritty" {
+					//不同窗口的按键逻辑在这
+					fmt.Println("hello alacritty")
+				}
+			case err := <-wayland.SubscriptionSwayIPC.Errors:
+				fmt.Println("Error:", err)
+			}
+		}
+	}()
+
+	for {
+		fmt.Println("adadad")
+		time.Sleep(time.Second)
+	}
 }
